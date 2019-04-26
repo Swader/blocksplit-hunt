b409a54ad5e942ea0e789cf7dc18d39407d8c908
Marin Petrunić <marin.petrunic@gmail.com>
2019-04-26 17:00:06 +0200

    new contract

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -9,7 +9,7 @@ export const activateLogger = true;
 // export const contractAddress = '0x345ca3e014aaf5dca488057592ee47305d9b3e10'; //Ganache
 // export const contractAddress = '0x25bfc3939528218a362918c7974a932a2a9b9537'; //Ropsten
  // export const contractAddress = '0xfb6a059c20bbf907bcbbddc656a6f831d7f0ae71'; //new Ropseten
- export const contractAddress = '0x7dfc7743e902f680e9bca47f7d3eec08194dd13d'; //Mainnet - feat/no-token-type
+ export const contractAddress = '0x42821ef5242b81e371391e6d988fcc7d0647bfbf'; //Mainnet - feat/no-token-type
 
 /**
  * Ethereum network
